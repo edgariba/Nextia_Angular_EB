3ce732af6e6f7fc99c1a899c5a71c7b936f6dcf5
Edgar Barrera <edgarlib123@gmail.com>
2022-07-12 12:48:38 -0500

    agregue los componentes services

---
+++ src/app/providers/constant-service/constants.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+@Injectable()
+export class ConstantsService {
+  server = environment.server;
+
+  constructor() {
+    console.log('Hello ConstantServiceProvider Provider');
+  }
+
+  getHeaders() {
+    let httpHeaders = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Access-Control-Allow-Headers', 'Content-Type')
+      .set('Access-Control-Allow-Origin', '*')
+    let options = {
+      headers: httpHeaders
+    };
+    return options;
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  server: "http://localhost:8080/api/v1/",
   production: true
 };
